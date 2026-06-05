fdeec742057a12c0781c086648f854c94380de20
Amine Mazigh <54552812+amine-12@users.noreply.github.com>
2026-06-04 21:17:55 -0400

    config

--- frontend/src/service/ApiService.tsx
+++ frontend/src/service/ApiService.tsx
@@ -1,6 +1,6 @@
 // services/analyseService.ts
 
-const API_BASE_URL = import.meta.env.VITE_API_URL
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000"
 
 export interface AnalyseResponse {
   success: boolean;
