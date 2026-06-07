73d1fed0ea2f32c09682919fd7333866c672a0c8
Jethro Roy <jethroroy@gmail.com>
2026-06-07 14:38:05 -0400

    Add prisma config in js for deployment

---
+++ backend/prisma.config.js
@@ -0,0 +1,11 @@
+require('dotenv').config({ path: '.env' });
+
+module.exports = {
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+};
